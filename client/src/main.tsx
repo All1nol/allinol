e9de4b85b98f77807c88ccb8941b8fe19d731612
All1nol <newacckim@gmail.com>
2025-03-16 15:24:10 +0400

    back to browser router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import { AppProvider } from './context/AppContext.tsx';
 import { TaskProvider } from './context/TaskContext.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
-import { HashRouter } from 'react-router-dom';
-// Import Tailwind CSS
 import './styles.css';
 
 // Create a client
@@ -23,7 +22,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <AuthProvider>
           <AppProvider>
             <TaskProvider>
@@ -31,7 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             </TaskProvider>
           </AppProvider>
         </AuthProvider>
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
